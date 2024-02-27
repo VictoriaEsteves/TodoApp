caa4c8d02fd06f9a62950ac767ea5cb8e10a3fd0
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2024-02-27 17:12:09 -0300

    :)

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+
+function handleKeyPress(event) {
+    console.log("Tecla pressionada:", event.keyCode);
+    if (event.keyCode === 13) { // Verifica se a tecla pressionada é a tecla "Enter"
+        addTask(); // Chama a função addTask() para adicionar uma nova tarefa
+    }
+}
+/*
+function handleKeyPress(event) {
+    if (event.keyCode === 13) { // Verifica se a tecla pressionada é a tecla "Enter"
+        addTask(); // Chama a função addTask() para adicionar uma nova tarefa
+    }
+}
+*/
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskText = taskInput.value.trim(); // Remove espaços em branco no início e no final
+
+    if (taskText !== "") {
+        var taskList = document.getElementById("taskList");
+        var newTaskItem = document.createElement("li");
+        newTaskItem.innerHTML = `<input type="checkbox" onchange="toggleTaskCompletion(this)"> ${taskText}`;
+        taskList.appendChild(newTaskItem);
+        taskInput.value = ""; // Limpar o campo de entrada após adicionar a tarefa
+    }
+}
