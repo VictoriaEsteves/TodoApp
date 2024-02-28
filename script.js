888c78b81dca55af66c7e750eb2a388406e86c99
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2024-02-27 21:54:30 -0300

    :P

--- script.js
+++ script.js
@@ -16,17 +16,27 @@ function handleKeyPress(event) {
 function addTask() {
     var taskInput = document.getElementById("taskInput");
     var taskText = taskInput.value.trim(); // Remove espaços em branco no início e no final
-
+    
     if (taskText !== "") {
         var taskList = document.getElementById("taskList");
         var newTaskItem = document.createElement("li");
-        newTaskItem.innerHTML = `<input type="checkbox" onchange="toggleTaskCompletion(this)"> ${taskText}`;
+        newTaskItem.innerHTML = `<label><input type="checkbox" onchange="toggleTaskCompletion(this)"> ${taskText}</label>`;
         taskList.appendChild(newTaskItem);
         taskInput.value = ""; // Limpar o campo de entrada após adicionar a tarefa
     }
+
 }
 
 function clearCompl() {
     var taskList = document.getElementById("taskList");
     taskList.innerHTML = "";
+}
+
+function toggleTaskCompletion(checkbox) {
+    var taskItem = checkbox.parentNode.parentNode; // Obtém o elemento li pai do checkbox
+    if (checkbox.checked) {
+        document.style.tes
+    } else {
+        taskItem.classList.remove("completed"); // Remove a classe "completed"
+    }
 }
